3fa3f30a81c40c39040077ccafb0337ea34635a3
fareedahamed0425-code <fareedahamed0425@gmail.com>
2026-08-28 22:48:26 +0530

    Enable reload in backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -499,4 +499,4 @@ async def download_temp_file(filename: str, background_tasks: BackgroundTasks):
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8008))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
